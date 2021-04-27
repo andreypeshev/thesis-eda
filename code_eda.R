1a3fd37409c582ad8d5ddab2521aa90e0961728e
Andrey Peshev <andreypeshev6@gmail.com>
2021-04-27 11:30:58 +0300

    paramters descr
    added description of parameters

--- code_eda.R
+++ code_eda.R
@@ -1,6 +1,12 @@
 # prb = Percentage Relative Bias
 # mcsd = Monte Carlo Standard Deviation
 # data gen equation: Y = 1.0 + 0.33X + 0.33Z1 + 0.33Z2 + ε
+# Simulation parameters:
+# Proportion of the MAR predictor represented in the analysis model: pMAR = {1.0, 0.75, 0.5, 0.25, 0.0}
+# Strength of correlations among the predictors: rXZ = {0.0, 0.1, 0.3, 0.5}
+# Sample size: N = {100, 250, 500}
+# Proportion of missing data: PM = {0.1, 0.2, 0.4}
+# R^2 for the data generating model: R^2 = {0.15, 0.3, 0.6}
 
 #clear the working enviornment 
 rm(list=ls())
@@ -39,9 +45,9 @@ data_prb[which.max(abs(data_prb$x)),]
 #couting the different number of conditions for slope PRB smaller than -10
 prb_x_smallneg <- data_prb[data_prb$x < -10, ]
 
-for (i in 1:length(prb_x_smallneg[1:7])){
-  print(ggplot(prb_x_smallneg, aes(x=prb_x[,i])) + geom_bar() + xlab(colnames(prb_x_smallneg[i])))
-}
+# for (i in 1:length(prb_x_smallneg[1:7])){
+#   print(ggplot(prb_x_smallneg, aes(x=prb_x[,i])) + geom_bar() + xlab(colnames(prb_x_smallneg[i])))
+# }
 
 
 data_prb[data_prb$x < 1 & data_prb$x > 0, ]
